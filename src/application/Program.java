36e2f7d1f5e13c2265336ac46bc54801589078e0
Luiz Covre <luiz.1covre@gmail.com>
2024-01-16 19:35:13 -0300

    implementação findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.VendedorDao;
-import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Departamento obj = new Departamento(1,"Livros");
-		
-		Vendedor vendedor = new Vendedor(1, "Luiz", "@gmail", new Date(), 2345.67, obj);
-		
 		VendedorDao vendedorDao = DaoFactory.createVendedorDao();
+		
+		Vendedor vendedor = vendedorDao.findById(3);
 		
 		System.out.println(vendedor);
 	}
--- src/model/dao/impl/VendedorDaoJDBC.java
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.VendedorDao;
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class VendedorDaoJDBC implements VendedorDao{
 
+	private Connection conn;
+	
+	public VendedorDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Vendedor prObj) {
 	}
@@ -21,7 +34,39 @@ public class VendedorDaoJDBC implements VendedorDao{
 
 	@Override
 	public Vendedor findById(Integer prId) {
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"select X.*, Y.Name as DepName "
+			      + "from seller as X "
+				  + "join department Y on (Y.Id = X.DepartmentId)"
+				  + "where X.Id = ?");
+			
+			st.setInt(1, prId);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Departamento depart = new Departamento();
+				depart.setwId(rs.getInt("DepartmentId"));
+				depart.setwNome(rs.getString("DepName"));
+				Vendedor obj = new Vendedor();
+				obj.setwId(rs.getInt("Id"));
+				obj.setwNome(rs.getString("Name"));
+				obj.setwEmail(rs.getString("Email"));
+				obj.setwSalBase(rs.getDouble("BaseSalary"));
+				obj.setwDataNasc(rs.getDate("BirthDate"));
+				obj.setwDepartamento(depart);
+				return obj;
+			}
+			return null; 
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
